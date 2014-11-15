64f5a3c123677c0092010ffb5df180412078f3f0
Neha Patki <nehapatki@gmail.com>
2014-11-15 17:52:03 -0500

    Begin writing feature extractors for data; untested.

---
+++ extractor.py
@@ -0,0 +1,143 @@
+"""
+A script that extracts features and writes a new file of feature
+vectors.
+"""
+import math
+
+
+class AmbianceExtractor(object):
+
+    all_ambiances = [
+        'casual',
+        'classy',
+        'divey',
+        'hipster',
+        'intimate',
+        'romantic',
+        'touristy',
+        'trendy',
+        'upscale'
+    ]
+
+    def __call__(self, data):
+        """Return binary feature vector with 1's that
+        correspond to the appropriate ambiances."""
+
+        vector = [0]*len(self.all_ambiances)
+
+        if 'Ambiance' in data['attributes']:
+            ambiances = data['attributes']['Ambiance']
+
+            for ambiance in ambiances:
+                try:
+                    incex = self.all_ambiances.index(ambiance)
+                except ValueError:
+                    continue
+                vector[index] = 1
+
+        return vector
+
+
+class BooleanAttributesExtractor(object):
+
+    # attribute that have True/False values
+    attributes = [
+        'Accepts Credit Cards',
+        'Delivery',
+        'Dogs Allowed',
+        'Good for Kids',
+        'Good For Groups',
+        'Has TV',
+        'Outdoor Seating',
+        'Waiter Service'
+    ]
+
+    def __call__(self, data):
+        vector = [0]*len(self.attributes)
+
+        for index, attribute in enumerate(self.attributes):
+            if attribute in data['attributes']:
+                vector[index] = 1
+
+        return vector
+
+
+class CategoryExtractor(object):
+
+    # These are all the categories that have >= 1000 businesses
+    # in the business data set
+    all_categories = [
+        'Restaurants',
+        'Shopping',
+        'Food',
+        'Beauty & Spas',
+        'Nightlife',
+        'Bars',
+        'Health & Medical',
+        'Automotive',
+        'Home Services',
+        'Fashion',
+        'Active Life',
+        'Mexican',
+        'Event Planning & Services',
+        'American (Traditional)',
+        'Fast Food',
+        'Local Services',
+        'Pizza',
+        'Hotels & Travel',
+        'Arts & Entertainment',
+        'Sandwiches',
+        'Coffee & Tea',
+        'American (New)',
+        'Italian',
+        'Chinese'
+    ]
+
+    def __call__(self, data):
+        """Return binary feature vector with 1's that 
+        corresponds to the appropriate categories."""
+
+        vector = [0]*len(self.all_categories)
+        
+        if 'categories' in data:
+            categories = data['categories']
+            for category in categories:
+                try:
+                    index = self.all_categories.index(category)
+                except ValueError:
+                    continue
+                vector[index] = 1
+
+        return vector
+
+
+class CityExtractor(object):
+
+    # Latitude, longitude locations for the 5 cities, as
+    # provided by Google. Note that + corresponds to North/East,
+    # - corresponds to South/West
+    locations = {
+        'Phoenix': (33.4500, -112.0667),
+        'Las Vegas': (36.1215, -115.1739),
+        'Madison': (43.0667, -89.4000),
+        'Waterloo': (43.4667, -80.5167),
+        'Edinburgh': (55.9531, -3.1889)
+    }
+
+    def __call__(self, data):
+        loc = (data['latitude'], data['longitude'])
+
+        best_guess_city = None
+        distance_guess = float('inf')
+
+        for city, center in self.location.items():
+            val = (center[0]-loc[0])**2 + (center[1]-loc[1])**2
+            if val < distance_guess:
+                distance_guess = val
+                best_guess_city = city
+
+        return best_guess_city
+
+
+def rating_extractor(data):
+    return [data['review_count'], data['stars']]
