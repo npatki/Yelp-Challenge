7b887e36bea723ca3cd9c56108e495256ece62bf
Neha Patki <nehapatki@gmail.com>
2014-11-18 12:58:51 -0500

    Partition only Restaurant businesses

--- partitioner.py
+++ partitioner.py
@@ -1,13 +1,16 @@
 """
 This is a script that splits the business data set randomly into
-4 partitions. There are a total of 42153 business data points,
-so we parition into sets of approximately 10538. It's deterministic.
-"""
+4 partitions. We only consider businesses that include 'Restaurants' in
+their category list.
 
+There are 14303 such businesses in the data set so we put 3575 or 3576
+businesses in each partition.
+"""
+import json
 import random
 
 BUSINESS = '../data/yelp_academic_dataset_business.json'
-MAX = 10538
+MAX = 3576
 OUT = [
     open('../data/biz_0.json', 'a+'),
     open('../data/biz_1.json', 'a+'),
@@ -16,25 +19,25 @@ OUT = [
 ]
 
 if __name__ == '__main__':
-    first = False
-
     # make it deterministic
     random.seed(0)
+
+    # keep track of how many businesses we've assigned to each partition
     counts = [0, 0, 0, 0]
 
     with open(BUSINESS) as f:
         for line in f:
-            if not first:
-                OUT[0].write(line)
-                first = True
-                continue
+            data = json.loads(line)
 
-            partition = random.randint(0, len(OUT)-1)
-            while counts[partition] >= MAX:
+            if 'Restaurants' in data['categories']:
                 partition = random.randint(0, len(OUT)-1)
 
-            OUT[partition].write(line)
-            counts[partition] += 1
+                # choose a partition that has spots available
+                while counts[partition] >= MAX:
+                    partition = random.randint(0, len(OUT)-1)
+
+                OUT[partition].write(line)
+                counts[partition] += 1
 
     for f in OUT:
         f.close()
